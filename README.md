95b688441506c3deab72b9bcd95f576e45039222
Tim <zqrk.pers@gmail.com>
2024-01-11 21:26:21 +0100

    Add WASM demo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # m1_acl
 Projet d'ACL
+
+## Build
+
+- `npm link ..\back\pkg\` and then add the dependency in `package.json`
+- `wasm-pack build --release`
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 
+import { greet } from "back"
+
 function App() {
 
   return (
     <div className="bg-[url('./background.png')] bg-cover h-screen text-center flex flex-col justify-center">
       <h1 className="font-tinybox text-7xl text-amber-400 pb-8">Pixel Belote</h1>
       <div className="flex flex-col items-center justify-center space-y-4 *:shadow hover:*:bg-black/20 *:rounded-lg *:text-3xl *:font-mc *:border *:py-4 *:max-w-2xl *:w-full *:backdrop-blur-sm">
-        <button>Play</button>
+        <button onClick={() => {greet("alex")}}>Play</button>
         <button>Rules</button>
         <button>Settings</button>
       </div>
